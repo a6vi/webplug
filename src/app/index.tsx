bc6b4c81b1556c7e3c4a7521f5161429dd9e98f4
changin <zh69ou@qq.com>
2026-09-20 13:49:58 +0800

    基础数据调整

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,3 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
 export default function HomeScreen() {
-  return <></>;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>页面 1</Text>
+      <Text>这是由 Expo 驱动的第一个原生页面。</Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: "center", alignItems: "center" },
+  title: { fontSize: 24, fontWeight: "bold", marginBottom: 16 },
+});
